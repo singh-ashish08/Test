243334678b9000efb54974c39497183fa55ab7c3
AshishSingh24356 <ashishsingh24356@gmail.com>
2025-01-15 12:38:55 +0530

    Modified file

--- src/com/hcl/www/EvenNumber.java
+++ src/com/hcl/www/EvenNumber.java
@@ -3,7 +3,7 @@ package com.hcl.www;
 public class EvenNumber {
 	public static void main(String[] args) {
 
-		int num = 56;
+		int num = 90;
 		if (num % 2 == 0) {
 			System.out.println("Even");
 		} else {
