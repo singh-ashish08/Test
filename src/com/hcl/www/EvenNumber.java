209e06d358ad122a67068b873b2aff2dd6107300
AshishSingh24356 <ashishsingh24356@gmail.com>
2025-01-15 12:31:54 +0530

    Second commit even number

---
+++ src/com/hcl/www/EvenNumber.java
@@ -0,0 +1,14 @@
+package com.hcl.www;
+
+public class EvenNumber {
+	public static void main(String[] args) {
+
+		int num = 56;
+		if (num % 2 == 0) {
+			System.out.println("Even");
+		} else {
+			System.out.println("Odd");
+		}
+
+	}
+}
