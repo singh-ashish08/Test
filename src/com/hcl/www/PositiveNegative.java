1d19e9c1766006b726ad2fbced88d0ea5b5acb52
singh-ashish08 <ashish.rathore.1997@gmail.com>
2025-01-16 14:05:08 +0530

    Testing with one more value

--- src/com/hcl/www/PositiveNegative.java
+++ src/com/hcl/www/PositiveNegative.java
@@ -2,7 +2,7 @@ package com.hcl.www;
 
 public class PositiveNegative {
 	public static void main(String[] args) {
-		int num = 24;
+		int num = -87;
 
 		if (num < 0) {
 			System.out.println("Negative number ");
