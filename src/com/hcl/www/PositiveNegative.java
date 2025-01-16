c7ac8a87d54a72628369c354f36d0c3322d556fc
singh-ashish08 <ashish.rathore.1997@gmail.com>
2025-01-16 13:06:52 +0530

    Adding one file positive and negative number

---
+++ src/com/hcl/www/PositiveNegative.java
@@ -0,0 +1,13 @@
+package com.hcl.www;
+
+public class PositiveNegative {
+	public static void main(String[] args) {
+		int num = 24;
+
+		if (num < 0) {
+			System.out.println("Negative number ");
+		} else {
+			System.out.println("Positive Number ");
+		}
+	}
+}
